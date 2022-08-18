b825d55116603b4c2e17d6193e8b87cdfc89c77f
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-18 20:40:53 -0300

    Initial commit

---
+++ Assets/Scripts/ObjectSelectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelectionManager : MonoBehaviour
+{
+    public GameObject selected;
+    public float movSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * movSpeed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * movSpeed * Time.deltaTime;
+
+        if (selected)
+        {
+        selected.transform.Translate(new Vector3(x,y,0),Space.World);
+        }
+    }
+}
---
+++ Assets/Scripts/SelectOnClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectOnClick : MonoBehaviour
+{
+    public ObjectSelectionManager selectionManager;
+    public Rigidbody2D rb;
+    public bool isKinematic = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectionManager = GameObject.FindGameObjectWithTag("SelectionManager").GetComponent<ObjectSelectionManager>();;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        
+        isKinematic = !isKinematic;
+        if (!isKinematic)
+        {
+            selectionManager.selected = gameObject;
+            rb.bodyType = RigidbodyType2D.Static;
+        }
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            selectionManager.selected = null;
+        }
+    }
+
+    
+}
